46fc676c474d9217e5e284f002e94b6c7e18ac5c
SteveKaneya <stevekaneya@gmail.com>
2021-01-04 00:07:18 +0300

    created stkPush request method, And model for storing transaction details

---
+++ database/migrations/2021_01_03_204110_create_mpesa_transactions_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMpesaTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mpesa_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('TransactionType');
+            $table->string('TransID');
+            $table->string('TransTime');
+            $table->decimal('TransAmount', 8,2);
+            $table->string('BusinessShortCode');
+            $table->string('BillRefNumber');
+            $table->string('InvoiceNumber');
+            $table->decimal('OrgAccountBalance',8,2);
+            $table->string('ThirdPartyTransID');
+            $table->string('MSISDN');
+            $table->string('FirstName');
+            $table->string('MiddleName');
+            $table->string('LastName');
+            $table->timestamps();
+        });
+    }
+//php artisan migrate:fresh - rolls back tables/previous migration then creates a new one.
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mpesa_transactions');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::get('/mpesa/password', 'App\Http\Controllers\MpesaController@LipaNaMpesaPassword'); //Testing whether the password has been generated.Test on Postman (http://127.0.0.1:8000/api/mpesa/password - run a get request on postman, this should then return a base-64 encoded password)
 Route::post('/mpesa/new/access/token', 'App\Http\Controllers\MpesaController@newAccessToken'); //Testing whether the token has been generated.Test on Postman (http://127.0.0.1:8000/api/mpesa/new/access/token)
+Route::post('/mpesa/stk/push', 'App\Http\Controllers\MpesaController@stkPush'); //Testing whether a pop up comes on phone asking you to key in mpesa pin.Test on Postman (http://127.0.0.1:8000/api/mpesa/stk/push)
